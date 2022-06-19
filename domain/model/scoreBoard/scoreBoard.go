a8514af004dc2ab2215a6e5e6738dfa8bed28d81
landcelita <73651295+landcelita@users.noreply.github.com>
2022-06-19 08:19:50 +0000

    ScoreBoardの実装

--- domain/model/scoreBoard/scoreBoard.go
+++ domain/model/scoreBoard/scoreBoard.go
@@ -1,6 +1,7 @@
 package scoreboard
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"mahjong/domain/model/score"
 )
@@ -21,9 +22,27 @@ func NewScoreBoard(
 	kyotaku			score.Score,
 	) (*ScoreBoard, error) {
 	
-	if score100k, _ := score.NewScore(1000000);
-	scores[0].Add(scores[1]).Add(scores[2]).Add(scores[3]).Add(kyotaku)
-	 {
-
+	if score100k, _ := score.NewScore(100000);
+	!scores[0].
+		Add(scores[1]).
+		Add(scores[2]).
+		Add(scores[3]).
+		Add(kyotaku).
+		Equals(*score100k){
+		
+		return nil, fmt.Errorf("合計scoreは100,000と等しくなければいけません。")
 	}
+
+	if zero, _ := score.NewScore(0);
+	kyotaku.LessThan(*zero){
+		return nil, fmt.Errorf("kyoutakuは0を下回ってはいけません。")
+	}
+
+	scoreBoard := ScoreBoard{
+		scoreBoardId:	scoreBoardId,
+		scores:			scores,
+		kyotaku:		kyotaku,
+	}
+
+	return &scoreBoard, nil
 }
