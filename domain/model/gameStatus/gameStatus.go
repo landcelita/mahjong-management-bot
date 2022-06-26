8e4658a20f4d4a8495de90b0917a71e406335c66
landcelita <73651295+landcelita@users.noreply.github.com>
2022-06-26 06:32:05 +0000

    NewGameStatusをprivateにした(Factoryでのみ生成したいので)

--- domain/model/gameStatus/gameStatus.go
+++ domain/model/gameStatus/gameStatus.go
@@ -2,31 +2,32 @@ package gamestatus
 
 import (
 	"fmt"
+
 	"github.com/google/uuid"
-	"github.com/landcelita/mahjong-management-bot/domain/model/baKyokuHonba"
-	"github.com/landcelita/mahjong-management-bot/domain/model/playerId"
-	"github.com/landcelita/mahjong-management-bot/domain/model/tonpuOrHanchan"
+	bakyokuhonba "github.com/landcelita/mahjong-management-bot/domain/model/baKyokuHonba"
 	"github.com/landcelita/mahjong-management-bot/domain/model/jicha"
+	playerid "github.com/landcelita/mahjong-management-bot/domain/model/playerId"
+	tonpuorhanchan "github.com/landcelita/mahjong-management-bot/domain/model/tonpuOrHanchan"
 )
 
 type GameStatusId uuid.UUID
 
 type (
 	GameStatus struct {
-		gameStatusId  	GameStatusId
-		baKyokuHonba	bakyokuhonba.BaKyokuHonba
-		tonpuOrHanchan 	tonpuorhanchan.TonpuOrHanchan
-		playerIds		map[jicha.Jicha]playerid.PlayerId
-		isActive		bool
+		gameStatusId   GameStatusId
+		baKyokuHonba   bakyokuhonba.BaKyokuHonba
+		tonpuOrHanchan tonpuorhanchan.TonpuOrHanchan
+		playerIds      map[jicha.Jicha]playerid.PlayerId
+		isActive       bool
 	}
 )
 
-func NewGameStatus(
-	gameStatusId	GameStatusId,
-	baKyokuHonba	bakyokuhonba.BaKyokuHonba,
-	tonpuOrHanchan	tonpuorhanchan.TonpuOrHanchan,
-	playerIds		map[jicha.Jicha]playerid.PlayerId,
-	isActive		bool,
+func newGameStatus(
+	gameStatusId GameStatusId,
+	baKyokuHonba bakyokuhonba.BaKyokuHonba,
+	tonpuOrHanchan tonpuorhanchan.TonpuOrHanchan,
+	playerIds map[jicha.Jicha]playerid.PlayerId,
+	isActive bool,
 ) (*GameStatus, error) {
 
 	if len(playerIds) != 4 {
@@ -39,24 +40,23 @@ func NewGameStatus(
 		}
 	}
 
-	if nan10, _ := bakyokuhonba.NewBaKyokuHonba(bakyokuhonba.Nan, 1, 0); 
-	tonpuOrHanchan == tonpuorhanchan.Tonpu &&
-	baKyokuHonba.IsLaterThanOrSameFor(*nan10) {
+	if nan10, _ := bakyokuhonba.NewBaKyokuHonba(bakyokuhonba.Nan, 1, 0); tonpuOrHanchan == tonpuorhanchan.Tonpu &&
+		baKyokuHonba.IsLaterThanOrSameFor(*nan10) {
 		return nil, fmt.Errorf("東風戦で南場に入ることはできません。")
 	}
-	
-	gameStatus := &GameStatus {
-		gameStatusId: gameStatusId,
-		baKyokuHonba: baKyokuHonba,
+
+	gameStatus := &GameStatus{
+		gameStatusId:   gameStatusId,
+		baKyokuHonba:   baKyokuHonba,
 		tonpuOrHanchan: tonpuOrHanchan,
-		playerIds: playerIds,
-		isActive: isActive,
+		playerIds:      playerIds,
+		isActive:       isActive,
 	}
 
 	return gameStatus, nil
 }
 
-func (gameStatus GameStatus) IsOlast() (bool) {
+func (gameStatus GameStatus) IsOlast() bool {
 	if gameStatus.tonpuOrHanchan == tonpuorhanchan.Tonpu {
 		if last, err := bakyokuhonba.NewBaKyokuHonba(
 			bakyokuhonba.Ton,
@@ -82,7 +82,7 @@ func (gameStatus GameStatus) IsOlast() (bool) {
 	return false
 }
 
-func (gameStatus *GameStatus) AdvanceGameBaKyoku() (error) {
+func (gameStatus *GameStatus) AdvanceGameBaKyoku() error {
 	if gameStatus.IsOlast() {
 		return fmt.Errorf("オーラス時にBaKyokuを進めることはできません。")
 	}
@@ -94,18 +94,22 @@ func (gameStatus *GameStatus) AdvanceGameBaKyoku() (error) {
 	gameStatus.playerIds[jicha.Pecha] = playerIds_copy[jicha.Toncha]
 
 	nextBaKyokuHonba, err := gameStatus.baKyokuHonba.IncrementBaKyoku()
-	
-	if err != nil { return err }
+
+	if err != nil {
+		return err
+	}
 
 	gameStatus.baKyokuHonba = *nextBaKyokuHonba
 
 	return nil
 }
 
-func (gameStatus *GameStatus) AdvanceGameHonba() (error) {
+func (gameStatus *GameStatus) AdvanceGameHonba() error {
 	nextBaKyokuHonba, err := gameStatus.baKyokuHonba.IncrementHonba()
 
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	gameStatus.baKyokuHonba = *nextBaKyokuHonba
 
