4913083840c201852c00aea222a527b78f372ef1
landcelita <73651295+landcelita@users.noreply.github.com>
2022-06-26 06:00:39 +0000

    First()のPtoP,PtoVの分離

--- testutil/testutil.go
+++ testutil/testutil.go
@@ -1,9 +1,17 @@
 package testutil
 
-func First[T any] (val *T, e error) T {
+func FirstPtoV[T any] (val *T, e error) T {
 	if(e != nil) {
 		panic(e)
 	}
 	
 	return *val
+}
+
+func FirstPtoP[T any] (val *T, e error) *T {
+	if(e != nil) {
+		panic(e)
+	}
+
+	return val
 }
